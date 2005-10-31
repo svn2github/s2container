650ae254873bb635591b4282c41e8860994a3074
koichik <koichik@319488c0-e101-0410-93bc-b5e51f62721a>
2005-10-31 17:15:15 +0000

    reorganized directory structures.
    git-svn-id: https://www.seasar.org/svn/s2container@72 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ trunk/s2-backport175/src/test/java/org/seasar/framework/container/factory/Hoge.java
@@ -0,0 +1,15 @@
+package org.seasar.framework.container.factory;
+
+/**
+ * @author higa
+ * @org.seasar.framework.container.annotation.backport175.Aspect(
+ *  value="aop.traceInterceptor",
+ *  pointcut="getAaa")
+ *
+ */
+public class Hoge {
+
+    public String getAaa() {
+        return null;
+    }
+}
