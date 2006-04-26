84f7e0f83c504c01a701b92b483b56c26aebf54a
manhole <manhole@319488c0-e101-0410-93bc-b5e51f62721a>
2006-04-26 10:02:22 +0000

    release: s2-backport175-2.3.8
    git-svn-id: https://www.seasar.org/svn/s2container@598 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ tags/2006-04-24/s2-backport175-2.3.8/src/test/java/org/seasar/framework/container/factory/Hoge4.java
@@ -0,0 +1,17 @@
+package org.seasar.framework.container.factory;
+
+public class Hoge4 {
+
+    /**
+     * @return
+     * @org.seasar.framework.container.annotation.backport175.Aspect(
+     *  "aop.traceInterceptor")
+     */
+    public String getAaa() {
+        return null;
+    }
+
+    public String getAaa(String aaa) {
+        return aaa;
+    }
+}
