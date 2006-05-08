82a2aeaad92a70bbab0b94b39648c6a10d32e999
higa <higa@319488c0-e101-0410-93bc-b5e51f62721a>
2006-05-08 08:24:53 +0000

    git-svn-id: https://www.seasar.org/svn/s2container@629 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ trunk/hotdeploy-demo/WEB-INF/src/examples/hotdeploy/dto/AddDto2.java
@@ -0,0 +1,34 @@
+package examples.hotdeploy.dto;
+
+public class AddDto2 {
+
+    private int arg1;
+    
+    private int arg2;
+    
+    private int result;
+
+    public int getArg1() {
+        return arg1;
+    }
+
+    public void setArg1(int arg1) {
+        this.arg1 = arg1;
+    }
+
+    public int getArg2() {
+        return arg2;
+    }
+
+    public void setArg2(int arg2) {
+        this.arg2 = arg2;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+}
