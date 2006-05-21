9774ac2cd35f9c1d30b39595a9ff8cf9ac028784
koichik <koichik@319488c0-e101-0410-93bc-b5e51f62721a>
2006-05-21 13:42:26 +0000

    merge from trunk r339:r342 merge from trunk r512:r513
    git-svn-id: https://www.seasar.org/svn/s2container@659 319488c0-e101-0410-93bc-b5e51f62721a

--- branches/seasar2-2.3.4-branch/s2-framework/src/main/java/org/seasar/framework/container/assembler/AbstractBindingTypeDef.java
+++ branches/seasar2-2.3.4-branch/s2-framework/src/main/java/org/seasar/framework/container/assembler/AbstractBindingTypeDef.java
@@ -78,11 +78,16 @@ public abstract class AbstractBindingTypeDef implements BindingTypeDef {
                 return true;
             }
         }
-        if (BindingUtil.isAutoBindable(propType)
-                && container.hasComponentDef(propType)) {
-            Object value = container.getComponent(propType);
-            setValue(componentDef, propertyDesc, component, value);
-            return true;
+        if (BindingUtil.isAutoBindable(propType)) {
+            if (container.hasComponentDef(propType)) {
+                Object value = container.getComponent(propType);
+                setValue(componentDef, propertyDesc, component, value);
+                return true;
+            }
+            if (propType.isAssignableFrom(ComponentDef.class)) {
+                setValue(componentDef, propertyDesc, component, componentDef);
+                return true;
+            }
         }
         return false;
     }
