9931c2c42bfd191985f59252bcce4f33d9830f9d
manhole <manhole@319488c0-e101-0410-93bc-b5e51f62721a>
2006-06-02 05:57:49 +0000

    release: s2-backport175-2.3.9
    git-svn-id: https://www.seasar.org/svn/s2container@748 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ tags/2006-06-02/s2-backport175-2.3.9/src/test/java/org/seasar/framework/container/factory/Hoge5.java
@@ -0,0 +1,10 @@
+package org.seasar.framework.container.factory;
+
+public class Hoge5 {
+
+    /**
+     * @org.seasar.framework.container.annotation.backport175.InitMethod
+     */
+    public void init() {
+    }
+}
