617154e302c8e94820829aa25985d5ab7ae7cf66
koichik <koichik@319488c0-e101-0410-93bc-b5e51f62721a>
2006-07-04 13:18:27 +0000

    initial import.
    git-svn-id: https://www.seasar.org/svn/s2container@912 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ trunk/diigu/org.seasar.diigu.eclipse.plugin/src/main/java/org/seasar/diigu/eclipse/DiiguPlugin.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.diigu.eclipse;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class DiiguPlugin extends AbstractUIPlugin {
+
+    public static final String PLUGIN_ID = "org.seasar.diigu.eclipse";
+
+    // The shared instance.
+    private static DiiguPlugin plugin;
+
+    /**
+     * The constructor.
+     */
+    public DiiguPlugin() {
+        plugin = this;
+    }
+
+    /**
+     * This method is called upon plug-in activation
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+    }
+
+    /**
+     * This method is called when the plug-in is stopped
+     */
+    public void stop(BundleContext context) throws Exception {
+        super.stop(context);
+        plugin = null;
+    }
+
+    /**
+     * Returns the shared instance.
+     */
+    public static DiiguPlugin getDefault() {
+        return plugin;
+    }
+
+    /**
+     * Returns an image descriptor for the image file at the given plug-in
+     * relative path.
+     * 
+     * @param path
+     *            the path
+     * @return the image descriptor
+     */
+    public static ImageDescriptor getImageDescriptor(String path) {
+        return AbstractUIPlugin.imageDescriptorFromPlugin("org.seasar.diigu",
+                path);
+    }
+}
