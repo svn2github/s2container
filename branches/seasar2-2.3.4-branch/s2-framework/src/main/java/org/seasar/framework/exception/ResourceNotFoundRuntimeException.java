426708e3f1ffdeaa414d865208b2224c221c228e
manhole <manhole@319488c0-e101-0410-93bc-b5e51f62721a>
2006-07-30 14:08:32 +0000

    move ResourceNotFoundRuntimeException from org.seasar.framework.util to org.seasar.framework.exception.
    git-svn-id: https://www.seasar.org/svn/s2container@1091 319488c0-e101-0410-93bc-b5e51f62721a

--- branches/seasar2-2.3.4-branch/s2-framework/src/main/java/org/seasar/framework/exception/ResourceNotFoundRuntimeException.java
+++ branches/seasar2-2.3.4-branch/s2-framework/src/main/java/org/seasar/framework/exception/ResourceNotFoundRuntimeException.java
@@ -13,22 +13,25 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.seasar.framework.util;
+package org.seasar.framework.exception;
+
 
 /**
  * @author higa
- * @author manhole
  * 
- * @deprecated use
- *             {@link org.seasar.framework.exception.ResourceNotFoundRuntimeException}
  */
-public class ResourceNotFoundRuntimeException extends
-        org.seasar.framework.exception.ResourceNotFoundRuntimeException {
+public class ResourceNotFoundRuntimeException extends SRuntimeException {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 9033370905740809950L;
+
+    private String path;
 
     public ResourceNotFoundRuntimeException(String path) {
-        super(path);
+        super("ESSR0055", new Object[] { path });
+        this.path = path;
     }
 
+    public String getPath() {
+        return path;
+    }
 }
