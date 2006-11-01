176fa818f13483c20a046a7e55d999c1571689d2
taichi <taichi@319488c0-e101-0410-93bc-b5e51f62721a>
2006-11-01 05:41:31 +0000

    小数点以下の桁数が多い浮動小数点のRDBへの読書きに関するテストを追加。
    git-svn-id: https://www.seasar.org/svn/s2container@1461 319488c0-e101-0410-93bc-b5e51f62721a

--- branches/seasar2-2.3.4-branch/s2-extension/src/test/java/org/seasar/extension/dataset/impl/XlsReaderTest.java
+++ branches/seasar2-2.3.4-branch/s2-extension/src/test/java/org/seasar/extension/dataset/impl/XlsReaderTest.java
@@ -31,14 +31,90 @@ import org.seasar.framework.util.TimestampConversionUtil;
  */
 public class XlsReaderTest extends S2TestCase {
 
+    public static class FloatingPointBean {
+        private Double column0;
+
+        private Double column1;
+
+        public FloatingPointBean(double column0, double column1) {
+            super();
+            this.column0 = new Double(column0);
+            this.column1 = new Double(column1);
+        }
+
+        public Double getColumn0() {
+            return column0;
+        }
+
+        public Double getColumn1() {
+            return column1;
+        }
+
+        public void setColumn0(Double column0) {
+            this.column0 = column0;
+        }
+
+        public void setColumn1(Double column1) {
+            this.column1 = column1;
+        }
+    }
+
     private static final String PATH = "org/seasar/extension/dataset/impl/XlsReaderImplTest.xls";
 
     private DataSet dataSet_;
+
+    protected void setUp() throws Exception {
+        dataSet_ = new XlsReader(PATH).read();
+        include("j2ee.dicon");
+    }
 
     public void testCreateTable() throws Exception {
         assertEquals("1", 6, dataSet_.getTableSize());
     }
 
+    public void testFloatingPoint() throws Exception {
+        DataTable table = dataSet_.getTable("FLOATING_POINT");
+        DataSet dataSet = new DataSetImpl();
+        dataSet.addTable(table);
+
+        FloatingPointBean[] beans = new FloatingPointBean[11];
+        beans[0] = new FloatingPointBean(0.1, 0.123);
+        beans[1] = new FloatingPointBean(0.01, 0.0123);
+        beans[2] = new FloatingPointBean(0.001, 0.00123);
+        beans[3] = new FloatingPointBean(0.0001, 0.000123);
+        beans[4] = new FloatingPointBean(0.00001, 0.0000123);
+        beans[5] = new FloatingPointBean(0.000001, 0.00000123);
+        beans[6] = new FloatingPointBean(0.0000001, 0.000000123);
+        beans[7] = new FloatingPointBean(0.00000001, 0.0000000123);
+        beans[8] = new FloatingPointBean(0.000000001, 0.00000000123);
+        beans[9] = new FloatingPointBean(0.0000000001, 0.000000000123);
+        beans[10] = new FloatingPointBean(0.00000000001, 0.0000000000123);
+
+        assertEquals(dataSet, beans);
+    }
+
+    public void testFloatingPointInsertTx() throws Exception {
+        DataTable table = dataSet_.getTable("FLOATING_POINT");
+        DataSet dataSet = new DataSetImpl();
+        dataSet.addTable(table);
+        deleteDb(dataSet);
+        writeDb(dataSet);
+        DataTable selected = readDbByTable("FLOATING_POINT");
+        assertEquals(table,selected);
+    }
+
+    public void testGetValue() throws Exception {
+        DataTable table = dataSet_.getTable(2);
+        DataRow row = table.getRow(0);
+        assertEquals("1", TimestampConversionUtil.toTimestamp("20040322",
+                "yyyyMMdd"), row.getValue(0));
+        assertEquals("2", new BigDecimal(123), row.getValue(1));
+        assertEquals("3", "\u3042", row.getValue(2));
+        assertEquals("4", "YWJj", Base64Util.encode((byte[]) row.getValue(3)));
+        assertEquals("5", new BigDecimal("0.05"), row.getValue(4));
+        assertEquals("6", Boolean.TRUE, row.getValue(5));
+    }
+    
     public void testSetupColumns() throws Exception {
         DataTable table = dataSet_.getTable(2);
         assertEquals("1", 6, table.getColumnSize());
@@ -69,71 +145,6 @@ public class XlsReaderTest extends S2TestCase {
         }
         DataTable table2 = dataSet_.getTable("EMPTY_TABLE");
         assertEquals("3", 0, table2.getRowSize());
-    }
-
-    public void testGetValue() throws Exception {
-        DataTable table = dataSet_.getTable(2);
-        DataRow row = table.getRow(0);
-        assertEquals("1", TimestampConversionUtil.toTimestamp("20040322",
-                "yyyyMMdd"), row.getValue(0));
-        assertEquals("2", new BigDecimal(123), row.getValue(1));
-        assertEquals("3", "\u3042", row.getValue(2));
-        assertEquals("4", "YWJj", Base64Util.encode((byte[]) row.getValue(3)));
-        assertEquals("5", new BigDecimal("0.05"), row.getValue(4));
-        assertEquals("6", Boolean.TRUE, row.getValue(5));
-    }
-
-    public void testFloatingPoint() throws Exception {
-        DataTable table = dataSet_.getTable("FLOATING_POINT");
-        DataSet dataSet = new DataSetImpl();
-        dataSet.addTable(table);
-
-        FloatingPointBean[] beans = new FloatingPointBean[11];
-        beans[0] = new FloatingPointBean(0.1, 0.123);
-        beans[1] = new FloatingPointBean(0.01, 0.0123);
-        beans[2] = new FloatingPointBean(0.001, 0.00123);
-        beans[3] = new FloatingPointBean(0.0001, 0.000123);
-        beans[4] = new FloatingPointBean(0.00001, 0.0000123);
-        beans[5] = new FloatingPointBean(0.000001, 0.00000123);
-        beans[6] = new FloatingPointBean(0.0000001, 0.000000123);
-        beans[7] = new FloatingPointBean(0.00000001, 0.0000000123);
-        beans[8] = new FloatingPointBean(0.000000001, 0.00000000123);
-        beans[9] = new FloatingPointBean(0.0000000001, 0.000000000123);
-        beans[10] = new FloatingPointBean(0.00000000001, 0.0000000000123);
-
-        assertEquals(dataSet, beans);
-    }
-
-    protected void setUp() throws Exception {
-        dataSet_ = new XlsReader(PATH).read();
-    }
-
-    public static class FloatingPointBean {
-        private Double column0;
-
-        private Double column1;
-
-        public FloatingPointBean(double column0, double column1) {
-            super();
-            this.column0 = new Double(column0);
-            this.column1 = new Double(column1);
-        }
-
-        public Double getColumn0() {
-            return column0;
-        }
-
-        public void setColumn0(Double column0) {
-            this.column0 = column0;
-        }
-
-        public Double getColumn1() {
-            return column1;
-        }
-
-        public void setColumn1(Double column1) {
-            this.column1 = column1;
-        }
     }
 
 }
