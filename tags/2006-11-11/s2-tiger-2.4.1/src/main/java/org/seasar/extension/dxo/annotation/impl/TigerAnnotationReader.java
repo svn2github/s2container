95f56536942c17158169e29ab7c971860919d7ef
higa <higa@319488c0-e101-0410-93bc-b5e51f62721a>
2006-11-11 10:35:09 +0000

    Copied remotely
    git-svn-id: https://www.seasar.org/svn/s2container@1579 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ tags/2006-11-11/s2-tiger-2.4.1/src/main/java/org/seasar/extension/dxo/annotation/impl/TigerAnnotationReader.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.dxo.annotation.impl;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.seasar.extension.dxo.annotation.AnnotationReader;
+import org.seasar.extension.dxo.annotation.ConversionRule;
+import org.seasar.extension.dxo.annotation.DatePattern;
+import org.seasar.extension.dxo.annotation.DxoConverter;
+import org.seasar.extension.dxo.annotation.TimePattern;
+import org.seasar.extension.dxo.annotation.TimestampPattern;
+import org.seasar.extension.dxo.converter.Converter;
+import org.seasar.framework.beans.BeanDesc;
+import org.seasar.framework.beans.PropertyDesc;
+import org.seasar.framework.beans.factory.BeanDescFactory;
+import org.seasar.framework.beans.util.BeanUtil;
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.util.tiger.AnnotationUtil;
+import org.seasar.framework.util.tiger.CollectionsUtil;
+
+/**
+ * @author koichik
+ */
+public class TigerAnnotationReader implements AnnotationReader {
+
+    protected S2Container container;
+
+    protected AnnotationReader next;
+
+    protected Map<Class<?>, Map<String, Converter>> convertersCache = Collections
+            .synchronizedMap(new HashMap<Class<?>, Map<String, Converter>>());
+
+    public TigerAnnotationReader(final S2Container container) {
+        this(container, null);
+    }
+
+    public TigerAnnotationReader(final S2Container container,
+            final AnnotationReader next) {
+        this.container = container.getRoot();
+        this.next = next;
+    }
+
+    @SuppressWarnings("unchecked")
+    public String getDatePattern(final Class dxoClass, final Method method) {
+        final DatePattern datePattern = getAnnotation(dxoClass, method,
+                DatePattern.class);
+        if (datePattern != null) {
+            return datePattern.value();
+        }
+        if (next != null) {
+            return next.getDatePattern(dxoClass, method);
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public String getTimePattern(final Class dxoClass, final Method method) {
+        final TimePattern timePattern = getAnnotation(dxoClass, method,
+                TimePattern.class);
+        if (timePattern != null) {
+            return timePattern.value();
+        }
+        if (next != null) {
+            return next.getTimePattern(dxoClass, method);
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public String getTimestampPattern(final Class dxoClass, final Method method) {
+        final TimestampPattern timestampPattern = getAnnotation(dxoClass,
+                method, TimestampPattern.class);
+        if (timestampPattern != null) {
+            return timestampPattern.value();
+        }
+        if (next != null) {
+            return next.getTimestampPattern(dxoClass, method);
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Map getConverters(final Class destClass) {
+        final Map<String, Converter> converters = convertersCache
+                .get(destClass);
+        if (converters != null) {
+            return converters;
+        }
+        return createConverters(destClass);
+    }
+
+    @SuppressWarnings("unchecked")
+    public String getConversionRule(final Class dxoClass, final Method method) {
+        final ConversionRule mapConversion = method
+                .getAnnotation(ConversionRule.class);
+        if (mapConversion != null) {
+            return mapConversion.value();
+        }
+        if (next != null) {
+            return next.getConversionRule(dxoClass, method);
+        }
+        return null;
+    }
+
+    protected <T extends Annotation> T getAnnotation(final Class<?> dxoClass,
+            final Method method, final Class<T> annotationType) {
+        final T annotation = method.getAnnotation(annotationType);
+        if (annotation != null) {
+            return annotation;
+        }
+        return dxoClass.getAnnotation(annotationType);
+    }
+
+    protected Map<String, Converter> createConverters(final Class<?> destClass) {
+        final Map<String, Converter> converters = CollectionsUtil.newHashMap();
+        final BeanDesc beanDesc = BeanDescFactory.getBeanDesc(destClass);
+        for (int i = 0; i < beanDesc.getPropertyDescSize(); ++i) {
+            final PropertyDesc propertyDesc = beanDesc.getPropertyDesc(i);
+            if (!propertyDesc.hasWriteMethod()) {
+                continue;
+            }
+            final Method method = propertyDesc.getWriteMethod();
+            for (final Annotation annotation : method.getDeclaredAnnotations()) {
+                final Class<? extends Annotation> annotationType = annotation
+                        .annotationType();
+                final Annotation metaAnnotation = annotationType
+                        .getAnnotation(DxoConverter.class);
+                if (metaAnnotation == null) {
+                    continue;
+                }
+                final DxoConverter dxoConverterAnnotation = DxoConverter.class
+                        .cast(metaAnnotation);
+                final String converterName = dxoConverterAnnotation.value();
+                final Converter converter = Converter.class.cast(container
+                        .getComponent(converterName));
+                final Map<?, ?> props = AnnotationUtil
+                        .getProperties(annotation);
+                BeanUtil.copyProperties(props, converter);
+                converters.put(propertyDesc.getPropertyName(), converter);
+                break;
+            }
+        }
+        convertersCache.put(destClass, converters);
+        return converters;
+    }
+
+}
---
+++ tags/2006-11-11/s2-tiger-2.4.1/src/main/java/org/seasar/framework/unit/S2TestIntrospector.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.unit;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+/**
+ * @author taedium
+ * 
+ */
+public interface S2TestIntrospector {
+
+    List<Method> getTestMethods(Class<?> testClass);
+
+    boolean isIgnored(Method method);
+
+    Class<? extends Throwable> expectedException(Method method);
+
+    long getTimeout(Method method);
+
+    List<String> getPrerequisiteExpressions(Class<?> testClass,
+            Method testMethod);
+
+    boolean needsTransaction(Class<?> testClass, Method testMethod);
+
+    boolean requiresTransactionCommitment(Class<?> testClass, Method testMethod);
+
+    List<Method> getBeforeClassMethods(Class<?> testClass);
+
+    List<Method> getAfterClassMethods(Class<?> testClass);
+
+    List<Method> getBeforeMethods(Class<?> testClass);
+
+    List<Method> getAfterMethods(Class<?> testClass);
+
+    Method getEachBeforeMethod(Class<?> testClass, Method testMethod);
+
+    Method getEachAfterMethod(Class<?> testClass, Method testMethod);
+
+}
