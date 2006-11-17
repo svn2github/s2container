f2a495e89be3d55898502782325382f913e23b19
koichik <koichik@319488c0-e101-0410-93bc-b5e51f62721a>
2006-11-17 11:02:30 +0000

    Seasar2.4.4 released.
    git-svn-id: https://www.seasar.org/svn/s2container@1636 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ tags/2006-11-17/s2-tiger-2.4.4/src/main/java/org/seasar/framework/unit/S2TestClassMethodsRunner.java
@@ -0,0 +1,279 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.unit;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runner.manipulation.Filter;
+import org.junit.runner.manipulation.Filterable;
+import org.junit.runner.manipulation.NoTestsRemainException;
+import org.junit.runner.manipulation.Sortable;
+import org.junit.runner.manipulation.Sorter;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunNotifier;
+import org.seasar.framework.container.annotation.tiger.Binding;
+import org.seasar.framework.container.annotation.tiger.BindingType;
+import org.seasar.framework.unit.impl.ConventionTestIntrospector;
+import org.seasar.framework.util.tiger.ReflectionUtil;
+
+/**
+ * @author taedium
+ * 
+ */
+public class S2TestClassMethodsRunner extends Runner implements Filterable,
+        Sortable {
+
+    private static class FailedBefore extends Exception {
+        private static final long serialVersionUID = 1L;
+    }
+
+    protected static Provider provider;
+
+    protected final List<Method> testMethods;
+
+    protected final Class<?> testClass;
+
+    public S2TestClassMethodsRunner(final Class<?> clazz) {
+        testClass = clazz;
+        testMethods = getTestMethods();
+    }
+
+    public static void dispose() {
+        provider = null;
+    }
+
+    @Override
+    public void run(final RunNotifier notifier) {
+        try {
+            runBefores(notifier);
+            if (testMethods.isEmpty()) {
+                testAborted(notifier, getDescription());
+            }
+            for (final Method method : testMethods) {
+                invokeTestMethod(method, notifier);
+            }
+        } catch (final FailedBefore e) {
+        } finally {
+            runAfters(notifier);
+        }
+    }
+
+    protected void runBefores(final RunNotifier notifier) throws FailedBefore {
+        try {
+            final List<Method> befores = getBeforeClassMethods();
+            for (final Method before : befores)
+                before.invoke(null);
+        } catch (final InvocationTargetException e) {
+            addFailure(e.getTargetException(), notifier);
+            throw new FailedBefore();
+        } catch (final Throwable e) {
+            addFailure(e, notifier);
+            throw new FailedBefore();
+        }
+    }
+
+    protected void runAfters(final RunNotifier notifier) {
+        final List<Method> afters = getAfterClassMethods();
+        for (final Method after : afters)
+            try {
+                after.invoke(null);
+            } catch (final InvocationTargetException e) {
+                addFailure(e.getTargetException(), notifier);
+            } catch (final Throwable e) {
+                addFailure(e, notifier);
+            }
+    }
+
+    protected void addFailure(final Throwable targetException,
+            final RunNotifier notifier) {
+
+        final Failure failure = new Failure(getDescription(), targetException);
+        notifier.fireTestFailure(failure);
+    }
+
+    protected void testAborted(final RunNotifier notifier,
+            final Description description) {
+        notifier.fireTestStarted(description);
+        notifier.fireTestFailure(new Failure(description, new Exception(
+                "No runnable methods")));
+        notifier.fireTestFinished(description);
+    }
+
+    protected List<Method> getTestMethods() {
+        return getProvider().getTestMethods(testClass);
+    }
+
+    protected List<Method> getBeforeClassMethods() {
+        return getProvider().getBeforeClassMethods(testClass);
+    }
+
+    protected List<Method> getAfterClassMethods() {
+        return getProvider().getAfterClassMethods(testClass);
+    }
+
+    @Override
+    public Description getDescription() {
+        final Description spec = Description.createSuiteDescription(getName());
+        for (final Method method : testMethods)
+            spec.addChild(methodDescription(method));
+        return spec;
+    }
+
+    protected String getName() {
+        return getTestClass().getName();
+    }
+
+    protected Object createTest() throws Exception {
+        return getTestClass().getConstructor().newInstance();
+    }
+
+    protected void invokeTestMethod(final Method method,
+            final RunNotifier notifier) {
+        Object test = null;
+        try {
+            test = createTest();
+        } catch (final Exception e) {
+            testAborted(notifier, methodDescription(method));
+            return;
+        }
+        createMethodRunner(test, method, notifier).run();
+    }
+
+    protected S2TestMethodRunner createMethodRunner(final Object test,
+            final Method method, RunNotifier notifier) {
+
+        return getProvider().createMethodRunner(test, method, notifier,
+                methodDescription(method));
+    }
+
+    protected String testName(final Method method) {
+        return method.getName();
+    }
+
+    protected Description methodDescription(final Method method) {
+        return Description.createTestDescription(getTestClass(),
+                testName(method));
+    }
+
+    public void filter(final Filter filter) throws NoTestsRemainException {
+        for (final Iterator<Method> iter = testMethods.iterator(); iter
+                .hasNext();) {
+            final Method method = iter.next();
+            if (!filter.shouldRun(methodDescription(method))) {
+                iter.remove();
+            }
+        }
+        if (testMethods.isEmpty()) {
+            throw new NoTestsRemainException();
+        }
+    }
+
+    public void sort(final Sorter sorter) {
+        Collections.sort(testMethods, new Comparator<Method>() {
+            public int compare(final Method o1, final Method o2) {
+                return sorter.compare(methodDescription(o1),
+                        methodDescription(o2));
+            }
+        });
+    }
+
+    protected Class<?> getTestClass() {
+        return testClass;
+    }
+
+    protected static Provider getProvider() {
+        if (provider == null) {
+            provider = new DefaultProvider();
+        }
+        return provider;
+    }
+
+    protected static void setProvider(final Provider p) {
+        provider = p;
+    }
+
+    public interface Provider {
+
+        List<Method> getTestMethods(Class<?> clazz);
+
+        List<Method> getBeforeClassMethods(Class<?> clazz);
+
+        List<Method> getAfterClassMethods(Class<?> clazz);
+
+        S2TestMethodRunner createMethodRunner(Object test, Method method,
+                RunNotifier notifier, Description description);
+    }
+
+    public static class DefaultProvider implements Provider {
+
+        protected S2TestIntrospector introspector;
+
+        protected Class<? extends S2TestMethodRunner> methodRunnerClass;
+
+        protected Constructor<? extends S2TestMethodRunner> constructor;
+
+        public DefaultProvider() {
+            final ConventionTestIntrospector conventionIntrospector = new ConventionTestIntrospector();
+            conventionIntrospector.init();
+            this.introspector = conventionIntrospector;
+            setTestMethodRunnerClass(S2TestMethodRunner.class);
+        }
+
+        @Binding(bindingType = BindingType.MAY)
+        public void setTestIntrospector(final S2TestIntrospector introspector) {
+            this.introspector = introspector;
+        }
+
+        @Binding(bindingType = BindingType.MAY)
+        public void setTestMethodRunnerClass(
+                final Class<? extends S2TestMethodRunner> methodRunnerClass) {
+
+            this.methodRunnerClass = methodRunnerClass;
+            this.constructor = ReflectionUtil.getConstructor(methodRunnerClass,
+                    Object.class, Method.class, RunNotifier.class,
+                    Description.class, S2TestIntrospector.class);
+        }
+
+        public List<Method> getTestMethods(final Class<?> clazz) {
+            return introspector.getTestMethods(clazz);
+        }
+
+        public List<Method> getBeforeClassMethods(final Class<?> clazz) {
+            return introspector.getBeforeClassMethods(clazz);
+        }
+
+        public List<Method> getAfterClassMethods(final Class<?> clazz) {
+            return introspector.getAfterClassMethods(clazz);
+        }
+
+        public S2TestMethodRunner createMethodRunner(final Object test,
+                final Method method, final RunNotifier notifier,
+                final Description description) {
+
+            return ReflectionUtil.newInstance(constructor, test, method,
+                    notifier, description, introspector);
+        }
+
+    }
+}
---
+++ tags/2006-11-17/s2-tiger-2.4.4/src/test/java/org/seasar/extension/dxo/annotation/impl/TigerAnnotationReaderTest.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.dxo.annotation.impl;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.List;
+import java.util.Map;
+
+import org.seasar.extension.dxo.annotation.AnnotationReaderFactory;
+import org.seasar.extension.dxo.annotation.ConversionRule;
+import org.seasar.extension.dxo.annotation.DatePattern;
+import org.seasar.extension.dxo.annotation.DxoConverter;
+import org.seasar.extension.dxo.converter.ConversionContext;
+import org.seasar.extension.dxo.converter.Converter;
+import org.seasar.extension.dxo.converter.impl.AbstractConverter;
+import org.seasar.framework.unit.S2FrameworkTestCase;
+
+/**
+ * @author koichik
+ */
+public class TigerAnnotationReaderTest extends S2FrameworkTestCase {
+
+    AnnotationReaderFactory factory;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        include(getClass().getName().replace('.', '/') + ".dicon");
+    }
+
+    public void testGetDateFormat() throws Exception {
+        assertEquals("class default", factory.getAnnotationReader()
+                .getDatePattern(
+                        Dxo.class,
+                        Dxo.class.getMethod("converted",
+                                new Class[] { List.class })));
+        assertEquals("foo", factory.getAnnotationReader().getDatePattern(
+                Dxo.class,
+                Dxo.class.getMethod("convert", new Class[] { Object.class })));
+        assertEquals("bar", factory.getAnnotationReader().getDatePattern(
+                Dxo.class,
+                Dxo.class.getMethod("convert", new Class[] { Number.class,
+                        String.class })));
+        assertEquals("baz", factory.getAnnotationReader().getDatePattern(
+                Dxo.class,
+                Dxo.class.getMethod("convert", new Class[] { Object[].class })));
+        assertNull(factory.getAnnotationReader().getDatePattern(Dxo2.class,
+                Dxo2.class.getMethod("convert", new Class[] { Object.class })));
+    }
+
+    public void testGetMapConversion() throws Exception {
+        assertEquals("map1", factory.getAnnotationReader().getConversionRule(
+                Dxo.class,
+                Dxo.class.getMethod("convert", new Class[] { Integer.class })));
+        assertEquals("map2", factory.getAnnotationReader().getConversionRule(
+                Dxo.class,
+                Dxo.class.getMethod("convert", new Class[] { Integer.class,
+                        Map.class })));
+        assertNull(factory.getAnnotationReader()
+                .getConversionRule(
+                        Dxo.class,
+                        Dxo.class.getMethod("convert",
+                                new Class[] { Integer[].class })));
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testGetConverters() throws Exception {
+        Map<String, Converter> converters = factory.getAnnotationReader()
+                .getConverters(Hoge.class);
+        assertNotNull(converters);
+        assertEquals(2, converters.size());
+
+        Converter converter = converters.get("foo");
+        assertNotNull(converter);
+        assertEquals("foo", converter.convert("foo", String.class, null));
+
+        converter = converters.get("bar");
+        assertNotNull(converter);
+        assertEquals("a_bar_b", converter.convert("bar", String.class, null));
+    }
+
+    @DatePattern("class default")
+    public interface Dxo {
+
+        List<?> converted(List<?> src);
+
+        @DatePattern("foo")
+        Object convert(Object src);
+
+        @DatePattern("bar")
+        void convert(Number src, String date);
+
+        @DatePattern("baz")
+        Object[] convert(Object[] src);
+
+        @ConversionRule("map1")
+        Map<?, ?> convert(Integer src);
+
+        @ConversionRule("map2")
+        void convert(Integer src, Map<?, ?> dest);
+
+        Map[] convert(Integer[] src);
+    }
+
+    public interface Dxo2 {
+        Object convert(Object src);
+    }
+
+    public static class Hoge {
+        protected String foo;
+
+        protected String bar;
+
+        public String getFoo() {
+            return foo;
+        }
+
+        @HogeDxoConverter
+        public void setFoo(String foo) {
+            this.foo = foo;
+        }
+
+        public String getBar() {
+            return bar;
+        }
+
+        @HogeDxoConverter(prefix = "a_", suffix = "_b")
+        public void setBar(String bar) {
+            this.bar = bar;
+        }
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    @DxoConverter("hogeDxoConverter")
+    public @interface HogeDxoConverter {
+        String prefix() default "";
+
+        String suffix() default "";
+    }
+
+    public static class HogeDxoConverterImpl extends AbstractConverter {
+        protected String prefix = "";
+
+        protected String suffix = "";
+
+        public void setPrefix(String prefix) {
+            this.prefix = prefix;
+        }
+
+        public void setSuffix(String suffix) {
+            this.suffix = suffix;
+        }
+
+        public Class<?> getDestClass() {
+            return Hoge.class;
+        }
+
+        public Class<?>[] getSourceClasses() {
+            return new Class[] { Hoge.class };
+        }
+
+        @SuppressWarnings("unchecked")
+        public Object convert(Object source, Class destClass,
+                ConversionContext context) {
+            return prefix + source + suffix;
+        }
+    }
+
+}
