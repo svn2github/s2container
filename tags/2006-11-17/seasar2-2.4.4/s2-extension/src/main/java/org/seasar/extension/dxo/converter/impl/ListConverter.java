f0904eb753b67a1902398f6b4ca6c9aeff2db772
koichik <koichik@319488c0-e101-0410-93bc-b5e51f62721a>
2006-11-17 11:02:03 +0000

    Seasar2.4.4 released.
    git-svn-id: https://www.seasar.org/svn/s2container@1635 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ tags/2006-11-17/seasar2-2.4.4/s2-extension/src/main/java/org/seasar/extension/dxo/converter/impl/ListConverter.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.dxo.converter.impl;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.seasar.extension.dxo.converter.ConversionContext;
+
+/**
+ * @author Satoshi Kimura
+ * @author koichik
+ */
+public class ListConverter extends AbstractConverter {
+
+    public Class[] getSourceClasses() {
+        return new Class[] { Object.class };
+    }
+
+    public Class getDestClass() {
+        return List.class;
+    }
+
+    public Object convert(final Object source, final Class destClass,
+            final ConversionContext context) {
+        if (source == null) {
+            return null;
+        }
+        if (source instanceof List) {
+            return source;
+        }
+        if (source.getClass().isArray()) {
+            return Arrays.asList((Object[]) source);
+        }
+        final List list = new ArrayList();
+        if (source instanceof Collection) {
+            list.addAll((Collection) source);
+        } else {
+            list.add(source);
+        }
+        return list;
+    }
+
+}
---
+++ tags/2006-11-17/seasar2-2.4.4/s2-framework/src/test/java/org/seasar/framework/container/hotdeploy/creator/web/aaa/HogeAction.java
@@ -0,0 +1,14 @@
+package org.seasar.framework.container.hotdeploy.creator.web.aaa;
+
+public class HogeAction {
+
+    private HogePage hogePage;
+
+    public HogePage getHogePage() {
+        return hogePage;
+    }
+
+    public void setHogePage(HogePage hogePage) {
+        this.hogePage = hogePage;
+    }
+}
