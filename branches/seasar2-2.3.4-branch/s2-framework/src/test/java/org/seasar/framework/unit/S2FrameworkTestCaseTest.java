2bbcd240688de912775040e3e996c4973064f5c9
manhole <manhole@319488c0-e101-0410-93bc-b5e51f62721a>
2007-01-22 02:46:00 +0000

    merged from trunk r1853:1854
    git-svn-id: https://www.seasar.org/svn/s2container@1858 319488c0-e101-0410-93bc-b5e51f62721a

--- branches/seasar2-2.3.4-branch/s2-framework/src/test/java/org/seasar/framework/unit/S2FrameworkTestCaseTest.java
+++ branches/seasar2-2.3.4-branch/s2-framework/src/test/java/org/seasar/framework/unit/S2FrameworkTestCaseTest.java
@@ -15,11 +15,17 @@
  */
 package org.seasar.framework.unit;
 
+import java.io.PrintWriter;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.servlet.ServletOutputStream;
+
+import junitx.framework.ArrayAssert;
+
 import org.seasar.framework.container.TooManyRegistrationRuntimeException;
+import org.seasar.framework.mock.servlet.MockHttpServletResponse;
 
 public class S2FrameworkTestCaseTest extends S2FrameworkTestCase {
 
@@ -116,6 +122,26 @@ public class S2FrameworkTestCaseTest extends S2FrameworkTestCase {
 
     public void testEmptyComponent() {
         include("empty.dicon");
+    }
+
+    public void testGetResponseString() throws Exception {
+        final MockHttpServletResponse res = getResponse();
+        final PrintWriter writer = res.getWriter();
+        writer.write("ZXCV");
+        writer.write("abc");
+
+        assertEquals("ZXCVabc", res.getResponseString());
+    }
+
+    public void testGetResponseBytes() throws Exception {
+        final MockHttpServletResponse res = getResponse();
+        final ServletOutputStream os = res.getOutputStream();
+        os.write(new byte[] { 3, 2, 1 });
+        os.write(new byte[] { 9, 8, 7, 6 });
+
+        final byte[] bytes = res.getResponseBytes();
+        assertEquals(7, bytes.length);
+        ArrayAssert.assertEquals(new byte[] { 3, 2, 1, 9, 8, 7, 6 }, bytes);
     }
 
     public static class Hoge {
