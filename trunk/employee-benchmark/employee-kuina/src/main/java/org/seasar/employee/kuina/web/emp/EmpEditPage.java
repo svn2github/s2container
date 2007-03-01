78c02cb26e1e53fa307848fdcac4c86c638d1d68
taichi <taichi@319488c0-e101-0410-93bc-b5e51f62721a>
2007-03-01 09:40:29 +0000

    git-svn-id: https://www.seasar.org/svn/s2container@1972 319488c0-e101-0410-93bc-b5e51f62721a

--- trunk/employee-benchmark/employee-kuina/src/main/java/org/seasar/employee/kuina/web/emp/EmpEditPage.java
+++ trunk/employee-benchmark/employee-kuina/src/main/java/org/seasar/employee/kuina/web/emp/EmpEditPage.java
@@ -1,6 +1,8 @@
 package org.seasar.employee.kuina.web.emp;
 
 import org.seasar.employee.kuina.web.CrudType;
+import org.seasar.teeda.extension.annotation.takeover.TakeOver;
+import org.seasar.teeda.extension.annotation.takeover.TakeOverType;
 import org.seasar.teeda.extension.annotation.validator.Required;
 
 public class EmpEditPage extends AbstractEmpPage {
@@ -17,6 +19,11 @@ public class EmpEditPage extends AbstractEmpPage {
 
 	public String prerender() {
 		return null;
+	}
+	
+	@TakeOver(type=TakeOverType.NEVER)
+	public String doPrevius() {
+		return "empList";
 	}
 
 	@Override
