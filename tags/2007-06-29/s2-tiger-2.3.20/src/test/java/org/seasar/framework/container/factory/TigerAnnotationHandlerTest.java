c897285267b2dea1e85e360009111093ff60dc4b
koichik <koichik@319488c0-e101-0410-93bc-b5e51f62721a>
2007-06-29 09:14:08 +0000

    Seasar2.3.20 リリース．
    git-svn-id: https://www.seasar.org/svn/s2container@2568 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ tags/2007-06-29/s2-tiger-2.3.20/src/test/java/org/seasar/framework/container/factory/TigerAnnotationHandlerTest.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.container.factory;
+
+import org.seasar.extension.unit.S2TestCase;
+import org.seasar.framework.beans.BeanDesc;
+import org.seasar.framework.beans.PropertyDesc;
+import org.seasar.framework.beans.factory.BeanDescFactory;
+import org.seasar.framework.container.AspectDef;
+import org.seasar.framework.container.AutoBindingDef;
+import org.seasar.framework.container.ComponentDef;
+import org.seasar.framework.container.DestroyMethodDef;
+import org.seasar.framework.container.IllegalDestroyMethodAnnotationRuntimeException;
+import org.seasar.framework.container.IllegalInitMethodAnnotationRuntimeException;
+import org.seasar.framework.container.InitMethodDef;
+import org.seasar.framework.container.InstanceDef;
+import org.seasar.framework.container.PropertyDef;
+import org.seasar.framework.container.assembler.AutoBindingDefFactory;
+import org.seasar.framework.container.deployer.InstanceDefFactory;
+
+/**
+ * @author higa
+ */
+public class TigerAnnotationHandlerTest extends S2TestCase {
+
+    private TigerAnnotationHandler handler = new TigerAnnotationHandler();
+
+    public void testCreateComponentDef() throws Exception {
+        assertNotNull("1", handler.createComponentDef(Hoge.class, null));
+        ComponentDef cd = handler.createComponentDef(Hoge2.class, null);
+        assertEquals("2", "aaa", cd.getComponentName());
+        assertEquals("3", "prototype", cd.getInstanceDef().getName());
+        assertEquals("4", "property", cd.getAutoBindingDef().getName());
+        ComponentDef cd2 = handler.createComponentDef(Hoge.class, InstanceDefFactory.REQUEST);
+        assertEquals("5", InstanceDef.REQUEST_NAME, cd2.getInstanceDef().getName());
+
+        ComponentDef cd9 = handler.createComponentDef(Hoge.class, InstanceDefFactory.REQUEST,
+                AutoBindingDefFactory.NONE);
+        assertEquals("20", AutoBindingDef.NONE_NAME, cd9.getAutoBindingDef().getName());
+
+        ComponentDef cd10 = handler.createComponentDef(Hoge2.class, InstanceDefFactory.REQUEST,
+                AutoBindingDefFactory.NONE);
+        assertEquals("21", AutoBindingDef.PROPERTY_NAME, cd10.getAutoBindingDef().getName());
+
+        ComponentDef cd11 = handler.createComponentDef(Hoge3.class, InstanceDefFactory.REQUEST,
+                AutoBindingDefFactory.NONE);
+        assertEquals("22", AutoBindingDef.PROPERTY_NAME, cd11.getAutoBindingDef().getName());
+    }
+
+    public void testCreatePropertyDef() throws Exception {
+        BeanDesc beanDesc = BeanDescFactory.getBeanDesc(Hoge.class);
+        PropertyDesc propDesc = beanDesc.getPropertyDesc("aaa");
+        assertNull("1", handler.createPropertyDef(beanDesc, propDesc));
+
+        beanDesc = BeanDescFactory.getBeanDesc(Hoge2.class);
+        propDesc = beanDesc.getPropertyDesc("aaa");
+        PropertyDef propDef = handler.createPropertyDef(beanDesc, propDesc);
+        assertEquals("2", "aaa", propDef.getPropertyName());
+        assertEquals("3", "aaa2", propDef.getExpression());
+
+        propDesc = beanDesc.getPropertyDesc("bbb");
+        propDef = handler.createPropertyDef(beanDesc, propDesc);
+        assertEquals("4", "none", propDef.getBindingTypeDef().getName());
+    }
+
+    public void testCreatePropertyDefForConstantAnnotation() throws Exception {
+        BeanDesc beanDesc = BeanDescFactory.getBeanDesc(Hoge.class);
+        PropertyDesc propDesc = beanDesc.getPropertyDesc("aaa");
+        assertNull("1", handler.createPropertyDef(beanDesc, propDesc));
+
+        beanDesc = BeanDescFactory.getBeanDesc(Hoge3.class);
+        propDesc = beanDesc.getPropertyDesc("aaa");
+        PropertyDef propDef = handler.createPropertyDef(beanDesc, propDesc);
+        assertEquals("2", "aaa", propDef.getPropertyName());
+        assertEquals("3", "aaa2", propDef.getExpression());
+
+        propDesc = beanDesc.getPropertyDesc("bbb");
+        propDef = handler.createPropertyDef(beanDesc, propDesc);
+        assertEquals("4", "none", propDef.getBindingTypeDef().getName());
+    }
+
+    public void testAppendAspect() throws Exception {
+        ComponentDef cd = handler.createComponentDef(Hoge.class, null);
+        handler.appendAspect(cd);
+        assertEquals("1", 1, cd.getAspectDefSize());
+        AspectDef aspectDef = cd.getAspectDef(0);
+        assertEquals("2", "aop.traceInterceptor", aspectDef.getExpression());
+    }
+
+    public void testAppendAspectForMethod() throws Exception {
+        ComponentDef cd = handler.createComponentDef(Hoge4.class, null);
+        handler.appendAspect(cd);
+        assertEquals("1", 1, cd.getAspectDefSize());
+        AspectDef aspectDef = cd.getAspectDef(0);
+        assertEquals("2", "aop.traceInterceptor", aspectDef.getExpression());
+        assertTrue("3", aspectDef.getPointcut().isApplied(Hoge4.class.getMethod("getAaa")));
+        assertFalse("4", aspectDef.getPointcut().isApplied(
+                Hoge4.class.getMethod("getAaa", new Class[] { String.class })));
+    }
+
+    public void testAppendAspectForConstantAnnotation() throws Exception {
+        ComponentDef cd = handler.createComponentDef(Hoge3.class, null);
+        handler.appendAspect(cd);
+        assertEquals("1", 1, cd.getAspectDefSize());
+        AspectDef aspectDef = cd.getAspectDef(0);
+        assertEquals("2", "aop.traceInterceptor", aspectDef.getExpression());
+    }
+
+    public void testAppendInitMethod() throws Exception {
+        ComponentDef cd = handler.createComponentDef(Hoge.class, null);
+        handler.appendInitMethod(cd);
+        assertEquals("1", 1, cd.getInitMethodDefSize());
+        InitMethodDef initMethodDef = cd.getInitMethodDef(0);
+        assertEquals("2", "init", initMethodDef.getMethodName());
+    }
+
+    public void testAppendInitMethodForConstantAnnotation() throws Exception {
+        ComponentDef cd = handler.createComponentDef(Hoge3.class, null);
+        handler.appendInitMethod(cd);
+        assertEquals("1", 1, cd.getInitMethodDefSize());
+        InitMethodDef initMethodDef = cd.getInitMethodDef(0);
+        assertEquals("2", "init", initMethodDef.getMethodName());
+    }
+
+    public void setUpAppendInitMethodForDicon() throws Exception {
+        include("TigerAnnotationHandlerTest.dicon");
+    }
+
+    public void testAppendInitMethodForDicon() throws Exception {
+        ComponentDef cd = getComponentDef(Hoge5.class);
+        assertEquals("1", 1, cd.getInitMethodDefSize());
+    }
+
+    public void testAppendInitMethodForException() throws Exception {
+        ComponentDef cd = handler.createComponentDef(Hoge6.class, null);
+        try {
+            handler.appendInitMethod(cd);
+            fail("1");
+        }
+        catch (IllegalInitMethodAnnotationRuntimeException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public void testAppendDestroyMethod() throws Exception {
+        ComponentDef cd = handler.createComponentDef(Hoge.class, null);
+        handler.appendDestroyMethod(cd);
+        assertEquals("1", 1, cd.getDestroyMethodDefSize());
+        DestroyMethodDef destroyMethodDef = cd.getDestroyMethodDef(0);
+        assertEquals("2", "destroy", destroyMethodDef.getMethodName());
+    }
+
+    public void testAppendDestroyMethodForConstantAnnotation() throws Exception {
+        ComponentDef cd = handler.createComponentDef(Hoge3.class, null);
+        handler.appendDestroyMethod(cd);
+        assertEquals("1", 1, cd.getDestroyMethodDefSize());
+        DestroyMethodDef destroyMethodDef = cd.getDestroyMethodDef(0);
+        assertEquals("2", "destroy", destroyMethodDef.getMethodName());
+    }
+
+    public void setUpAppendDestroyMethodForDicon() throws Exception {
+        include("TigerAnnotationHandlerTest.dicon");
+    }
+
+    public void testAppendDestroyMethodForDicon() throws Exception {
+        ComponentDef cd = getComponentDef(Hoge5.class);
+        assertEquals("1", 1, cd.getDestroyMethodDefSize());
+    }
+
+    public void testAppendDestroyMethodForException() throws Exception {
+        ComponentDef cd = handler.createComponentDef(Hoge6.class, null);
+        try {
+            handler.appendDestroyMethod(cd);
+            fail("1");
+        } catch (IllegalDestroyMethodAnnotationRuntimeException ex) {
+            System.out.println(ex);
+        }
+    }
+}
