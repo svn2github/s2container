602185422a74fb2978ce9f8efae98b6a9323c5d3
koichik <koichik@319488c0-e101-0410-93bc-b5e51f62721a>
2007-06-29 09:14:23 +0000

    Seasar2.3.20 リリース．
    git-svn-id: https://www.seasar.org/svn/s2container@2569 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ tags/2007-06-29/s2-backport175-2.3.20/src/test/java/org/seasar/framework/container/factory/Hoge.java
@@ -0,0 +1,20 @@
+package org.seasar.framework.container.factory;
+
+/**
+ * @author higa
+ * @org.seasar.framework.container.annotation.backport175.Aspect( value="aop.traceInterceptor",
+ *                                                                pointcut="getAaa")
+ * 
+ */
+public class Hoge {
+
+    public String getAaa() {
+        return null;
+    }
+
+    /**
+     * @org.seasar.framework.container.annotation.backport175.InitMethod
+     */
+    public void init() {
+    }
+}
