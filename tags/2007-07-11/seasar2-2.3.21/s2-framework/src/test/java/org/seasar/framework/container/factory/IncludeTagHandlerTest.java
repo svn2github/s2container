bb23cf72c7b1e400d54208db4b17b5f1400205f7
koichik <koichik@319488c0-e101-0410-93bc-b5e51f62721a>
2007-07-11 11:19:31 +0000

    Seasar2.3.21 リリース．
    git-svn-id: https://www.seasar.org/svn/s2container@2627 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ tags/2007-07-11/seasar2-2.3.21/s2-framework/src/test/java/org/seasar/framework/container/factory/IncludeTagHandlerTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.container.factory;
+
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import org.seasar.framework.container.S2Container;
+
+/**
+ * @author higa
+ * 
+ */
+public class IncludeTagHandlerTest extends TestCase {
+
+    private static final String PATH = "org/seasar/framework/container/factory/IncludeTagHandlerTest.dicon";
+
+    private static final String PATH2 = "org/seasar/framework/container/factory/aaa.dicon";
+
+    public void testInclude() throws Exception {
+        S2Container container = S2ContainerFactory.create(PATH);
+        assertEquals("1", new Date(0), container.getComponent(Date.class));
+    }
+
+    public void testInclude2() throws Exception {
+        S2Container container = S2ContainerFactory.create(PATH2);
+        assertSame("1", container.getComponent("aaa.cdate"), container
+                .getComponent("bbb.cdate"));
+    }
+
+    public void testInclude3() throws Exception {
+        S2Container container = S2ContainerFactory.create(PATH);
+        S2Container grandChild = (S2Container) container
+                .getComponent("grandChild");
+        S2Container child = (S2Container) container.getComponent("child");
+        S2Container grandChild2 = (S2Container) child
+                .getComponent("grandChild");
+
+        assertSame("1", grandChild, grandChild2);
+    }
+}
---
+++ tags/2007-07-11/seasar2-2.3.21/s2-framework/src/test/java/org/seasar/framework/util/DateConversionUtilTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import junit.framework.TestCase;
+
+public class DateConversionUtilTest extends TestCase {
+
+    public void testRemoveDelimiter() throws Exception {
+        assertEquals("1", "yyyyMMdd", DateConversionUtil
+                .removeDelimiter("yyyy/MM/dd"));
+    }
+
+    public void testGetDateFormat() throws Exception {
+        SimpleDateFormat sdf = DateConversionUtil.getDateFormat("2004/11/7",
+                Locale.JAPAN);
+        assertEquals("1", "yyyy/MM/dd", sdf.toPattern());
+    }
+
+    public void testGetDateFormat2() throws Exception {
+        SimpleDateFormat sdf = DateConversionUtil.getDateFormat("04/11/7",
+                Locale.JAPAN);
+        assertEquals("1", "yy/MM/dd", sdf.toPattern());
+    }
+
+    public void testGetDateFormat3() throws Exception {
+        SimpleDateFormat sdf = DateConversionUtil.getDateFormat("20041107",
+                Locale.JAPAN);
+        assertEquals("1", "yyyyMMdd", sdf.toPattern());
+    }
+
+    public void testGetDateFormat4() throws Exception {
+        SimpleDateFormat sdf = DateConversionUtil.getDateFormat("041107",
+                Locale.JAPAN);
+        assertEquals("1", "yyMMdd", sdf.toPattern());
+    }
+
+    public void testGetPattern() throws Exception {
+        System.out.println(DateConversionUtil.getPattern(Locale.JAPAN));
+    }
+
+    public void testGetDefaultPattern() throws Exception {
+        System.out.println(DateConversionUtil.getY4Pattern(Locale.JAPAN));
+    }
+}
