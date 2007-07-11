e3bfd29dbbdaa6b592e6ac03f8c44df036830708
koichik <koichik@319488c0-e101-0410-93bc-b5e51f62721a>
2007-07-11 11:19:44 +0000

    Seasar2.3.21 リリース．
    git-svn-id: https://www.seasar.org/svn/s2container@2628 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ tags/2007-07-11/s2-backport175-2.3.21/src/test/java/org/seasar/framework/container/factory/Hoge6.java
@@ -0,0 +1,10 @@
+package org.seasar.framework.container.factory;
+
+public class Hoge6 {
+
+    /**
+     * @org.seasar.framework.container.annotation.backport175.InitMethod
+     */
+    public void init(String s) {
+    }
+}
