1fc1bfbcb1ba4f1f40d4f9f3c35ed602308a181a
koichik <koichik@319488c0-e101-0410-93bc-b5e51f62721a>
2007-07-30 12:10:11 +0000

    Seasar2.4.16 リリース．
    git-svn-id: https://www.seasar.org/svn/s2container@2672 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ tags/2007-07-30/s2-tiger-2.4.16/src/main/java/org/seasar/framework/unit/InternalTestContext.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.unit;
+
+import java.lang.reflect.Method;
+
+import org.seasar.framework.aop.interceptors.MockInterceptor;
+import org.seasar.framework.container.AspectDef;
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.convention.NamingConvention;
+
+/**
+ * S2JUnit4の内部的なテストコンテキストです。
+ * <p>
+ * テストクラスから直接利用してはいけません。
+ * </p>
+ * 
+ * @author taedium
+ */
+public interface InternalTestContext extends TestContext {
+
+    /**
+     * テストクラスを設定します。
+     * 
+     * @param testClass
+     *            テストクラス
+     */
+    void setTestClass(Class<?> testClass);
+
+    /**
+     * テストメソッドを設定します。
+     * 
+     * @param testMethod
+     *            テストメソッド
+     */
+    void setTestMethod(Method testMethod);
+
+    /**
+     * 命名規約を設定します。
+     * 
+     * @param namingConvention
+     *            命名規約
+     */
+    void setNamingConvention(NamingConvention namingConvention);
+
+    /**
+     * S2コンテナを初期化します。
+     * 
+     */
+    void initContainer();
+
+    /**
+     * S2コンテナを破棄します。
+     * 
+     */
+    void destroyContainer();
+
+    /**
+     * 設定ファイルをインクルードします。
+     * 
+     */
+    void include();
+
+    /**
+     * テスト用のデータを準備します。
+     * 
+     */
+    void prepareTestData();
+
+    /**
+     * テストのコンテキストで利用するコンテナを返します。
+     * 
+     * @return コンテナ
+     */
+    S2Container getContainer();
+
+    /**
+     * モックインターセプターを登録します。
+     * 
+     * @param mockInterceptor
+     *            モックインターセプター
+     */
+    void addMockInterceptor(MockInterceptor mockInterceptor);
+
+    /**
+     * コンテナから<code>componentKey</code>をキーにして取得できるコンポーネント定義に<code>aspectDef</code>で表されるアスペクト定義を追加します。
+     * 
+     * @param componentKey
+     *            コンポーネントのキー
+     * @param aspectDef
+     *            アスペクト定義
+     */
+    void addAspecDef(Object componentKey, AspectDef aspectDef);
+
+    /**
+     * EJB3が使用可能の場合<code>true</code>を返します。
+     * 
+     * @return EJB3が使用可能の場合<code>true</code>、そうでない場合<code>false</code>
+     */
+    boolean isEjb3Enabled();
+
+    /**
+     * JTAが使用可能の場合<code>true</code>を返します。
+     * 
+     * @return JTAが使用可能の場合<code>true</code>、そうでない場合<code>false</code>
+     */
+    boolean isJtaEnabled();
+}
---
+++ tags/2007-07-30/s2-tiger-2.4.16/src/main/java/org/seasar/framework/unit/impl/ConfigFileIncluderImpl.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.unit.impl;
+
+import java.util.List;
+
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.container.annotation.tiger.Binding;
+import org.seasar.framework.container.annotation.tiger.BindingType;
+import org.seasar.framework.container.factory.S2ContainerFactory;
+import org.seasar.framework.log.Logger;
+import org.seasar.framework.unit.ConfigFileIncluder;
+import org.seasar.framework.unit.TestContext;
+import org.seasar.framework.util.ResourceUtil;
+import org.seasar.framework.util.tiger.CollectionsUtil;
+
+/**
+ * コンポーネント定義の設定ファイルをS2コンテナにインクルードする実装クラスです。
+ * <p>
+ * 設定ファイルはテストコンテキスト内のS2コンテナではなく、このオブジェクトに持つS2コンテナにインクルードします。
+ * </p>
+ * 
+ * @author taedium
+ */
+public class ConfigFileIncluderImpl implements ConfigFileIncluder {
+
+    /** ロガー */
+    protected static final Logger logger = Logger
+            .getLogger(ConfigFileIncluderImpl.class);
+
+    /** S2コンテナ */
+    protected S2Container container;
+
+    /** 設定ファイルのパスのリスト */
+    protected final List<String> configFiles = CollectionsUtil.newArrayList();
+
+    /**
+     * S2コンテナを設定します。
+     * 
+     * @param container
+     *            S2コンテナ
+     */
+    @Binding(bindingType = BindingType.MUST)
+    public void setContainer(S2Container container) {
+        this.container = container;
+    }
+
+    /**
+     * 設定ファイルのパスを登録します。
+     * 
+     * @param configFile
+     *            設定ファイルのパス
+     */
+    public void addConfigFile(final String configFile) {
+        configFiles.add(configFile);
+    }
+
+    public void include(final TestContext testContext) {
+        final String dirPath = testContext.getTestClassPackagePath();
+        for (final String configFile : configFiles) {
+            if (ResourceUtil.isExist(configFile)) {
+                include(testContext, configFile);
+            } else {
+                final String path = dirPath + "/" + configFile;
+                if (ResourceUtil.isExist(path)) {
+                    include(testContext, path);
+                }
+            }
+        }
+    }
+
+    /**
+     * 設定ファイルをS2コンテナにインクルードします。
+     * 
+     * @param testContext
+     *            テストコンテキスト
+     * @param path
+     *            設定ファイルのパス
+     */
+    protected void include(@SuppressWarnings("unused")
+    final TestContext testContext, final String path) {
+        if (logger.isDebugEnabled()) {
+            logger.log("DSSR0101", new Object[] { path });
+        }
+        S2ContainerFactory.include(container, path);
+    }
+
+}
