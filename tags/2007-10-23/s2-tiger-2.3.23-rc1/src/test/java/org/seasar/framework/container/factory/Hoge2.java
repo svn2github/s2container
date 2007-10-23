138e25ab0f04f40e5b92a76973c2fd277181aad3
koichik <koichik@319488c0-e101-0410-93bc-b5e51f62721a>
2007-10-23 09:16:44 +0000

    Seasar2.3.23-rc1 リリース．
    git-svn-id: https://www.seasar.org/svn/s2container@2981 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ tags/2007-10-23/s2-tiger-2.3.23-rc1/src/test/java/org/seasar/framework/container/factory/Hoge2.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.container.factory;
+
+import org.seasar.framework.container.annotation.tiger.AutoBindingType;
+import org.seasar.framework.container.annotation.tiger.Binding;
+import org.seasar.framework.container.annotation.tiger.BindingType;
+import org.seasar.framework.container.annotation.tiger.Component;
+import org.seasar.framework.container.annotation.tiger.InstanceType;
+
+@Component(name = "aaa", instance = InstanceType.PROTOTYPE, autoBinding = AutoBindingType.PROPERTY)
+public class Hoge2 {
+
+    @Binding("aaa2")
+    public void setAaa(String aaa) {
+    }
+
+    @Binding(bindingType = BindingType.NONE)
+    public void setBbb(String bbb) {
+    }
+
+    @Binding
+    public void setCcc(String ccc) {
+    }
+}
