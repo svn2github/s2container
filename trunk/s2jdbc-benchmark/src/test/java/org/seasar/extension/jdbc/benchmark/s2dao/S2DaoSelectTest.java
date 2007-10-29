6bd4ec2ef36d0a948948452f791412d9c61b2a9a
taedium <taedium@319488c0-e101-0410-93bc-b5e51f62721a>
2007-10-29 16:34:59 +0000

    S2Daoと素のJDBCの1万件SELECTのベンチマークのコードを追加しました。
    git-svn-id: https://www.seasar.org/svn/s2container@3063 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ trunk/s2jdbc-benchmark/src/test/java/org/seasar/extension/jdbc/benchmark/s2dao/S2DaoSelectTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.jdbc.benchmark.s2dao;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+import org.seasar.extension.jdbc.benchmark.AbstractSelectTest;
+import org.seasar.framework.container.SingletonS2Container;
+
+/**
+ * @author taedium
+ * 
+ */
+public class S2DaoSelectTest extends AbstractSelectTest {
+
+    private EmployeeDao employeeDao;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        employeeDao = SingletonS2Container.getComponent(EmployeeDao.class);
+    }
+
+    /**
+     * 
+     * @throws Exception
+     */
+    public void test() throws Exception {
+        userTransaction.begin();
+        long start = System.nanoTime();
+        List<Employee> employees = employeeDao.select();
+        long end = System.nanoTime();
+        userTransaction.commit();
+        assertEquals(10000, employees.size());
+        DecimalFormat df = new DecimalFormat("#,##0");
+        System.out.printf("%14s (nanoTime)\n", df.format(end - start));
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        employeeDao = null;
+        super.tearDown();
+    }
+}
